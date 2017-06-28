2b8a60d15ebcdfbee9d570182127d7a218f4075d
Michael Ledin <m.ledin@appheads.ru>
2017-06-28 11:20:56 +0300

    Simplify.

--- macro/src/main/scala/io/getquill/example/genericInsertOrUpdate/InsertOrUpdateMacro.scala
+++ macro/src/main/scala/io/getquill/example/genericInsertOrUpdate/InsertOrUpdateMacro.scala
@@ -9,15 +9,12 @@ class InsertOrUpdateMacro(val c: MacroContext) {
   def insertOrUpdate[T](entity: Tree, filter: Tree)(implicit t: WeakTypeTag[T]): Tree =
     q"""
       import ${c.prefix}._
-      val updateQuery = ${c.prefix}.quote {
+      if (run(${c.prefix}.quote {
         ${c.prefix}.query[$t].filter($filter).update(lift($entity))
-      }
-      val insertQuery = quote {
-        query[$t].insert(lift($entity))
-      }
-      run(${c.prefix}.query[$t].filter($filter)).size match {
-          case 1 => run(updateQuery)
-          case _ => run(insertQuery)
+      }) == 0) {
+          run(quote {
+            query[$t].insert(lift($entity))
+          })
       }
       ()
     """
